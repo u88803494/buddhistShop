edc65fa7cb913c6d4297f9fc87aa16875ac2f239
Henry Lee <u88803494@gmail.com>
2025-06-09 17:00:43 +0800

    Update HotProductsSection component to enhance layout with new styles and content, including a title and description for improved user engagement.

--- src/app/components/HotProductsSection.tsx
+++ src/app/components/HotProductsSection.tsx
@@ -1,6 +1,19 @@
 const HotProductsSection = () => {
   return (
-    <div>HotProductsSection</div>
+    <>
+    <div className="w-full h-72 bg-[#F2F2F2] flex flex-col items-center justify-center gap-2">
+        <h3 className="mb-8 text-2xl font-bold text-[#545454]">
+          跨越時空 在人與人之間蔓延
+        </h3>
+        <small className="flex flex-col items-center justify-center text-sm text-[#939295]">
+          <p>「靜思」緣起</p>
+          <p>原來，一個心念，有無盡的力量……</p>
+        </small>
+      </div>
+      <div>
+        熱門商品
+      </div>
+    </>
   )
 }
 
