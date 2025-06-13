0262b06a6b2f290a6c711390c6fcc0c2d40a4cff
Henry Lee <u88803494@gmail.com>
2025-06-13 13:14:55 +0800

    refactor: Update ProductListPage component for improved product filtering and pagination - Renamed component for consistency, adjusted product filtering logic, and ensured accurate total product count for pagination.

--- src/app/productList/page.tsx
+++ src/app/productList/page.tsx
@@ -8,24 +8,18 @@ import ProductGrid from "./components/ProductGrid";
 import ProductPagination from "./components/ProductPagination";
 import clsx from "clsx";
 
-const productListPage = () => {
-  const [selectedCategory, setSelectedCategory] = useState("all");
-
-  // 篩選分類
-  const filteredProducts =
-    selectedCategory === "all" ? products : products.filter((p) => p.category === selectedCategory);
-
+const ProductListPage = () => {
   // 假分頁
   const pageSize = 12;
   const [page, setPage] = useState(1);
-  const totalPage = Math.ceil(filteredProducts.length / pageSize);
-  const pagedProducts = filteredProducts.slice((page - 1) * pageSize, page * pageSize);
+  const totalPage = Math.ceil(products.length / pageSize);
+  const pagedProducts = products.slice((page - 1) * pageSize, page * pageSize);
 
   return (
     <div className={clsx("flex flex-col lg:flex-row", "min-h-screen")}>
       <CategorySidebar />
       <main className={clsx("mt-4 lg:mt-[86px]", "flex-1", "px-5 lg:px-0")}>
-        <ProductListHeader totalProducts={filteredProducts.length} />
+        <ProductListHeader totalProducts={products.length} />
         <ProductGrid products={pagedProducts} />
         <div className="my-20">
           <ProductPagination currentPage={page} totalPages={totalPage} onPageChange={setPage} />
@@ -35,4 +29,4 @@ const productListPage = () => {
   );
 };
 
-export default productListPage;
+export default ProductListPage;
